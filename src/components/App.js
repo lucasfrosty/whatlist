bf192b15f5858ff8ce2913b2ed461450ed71bcd6
lucasfrosty <lucas_frosty@hotmail.com>
2018-01-05 16:36:32 -0300

    transforming app into stateless component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // import ProtectedRoute from './ProtectedRoute';
@@ -6,20 +6,16 @@ import Home from './Home/Home';
 import Navbar from './Navbar/Navbar';
 import Details from './Details/Details';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Fragment>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/details/:type/:id" component={Details} />
-          </Switch>
-        </Fragment>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/details/:type/:id" component={Details} />
+      </Switch>
+    </Fragment>
+  </BrowserRouter>
+);
 
 export default App;
