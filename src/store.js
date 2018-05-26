36e2d667e93fccab2dbd7cd20f82f04201836569
lucasfrosty <lucas_frosty@hotmail.com>
2018-05-26 09:25:25 -0300

    removing redux-saga from dependencies

--- src/store.js
+++ src/store.js
@@ -1,27 +1,20 @@
-import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import throttle from 'lodash/throttle';
 
 import { loadState, saveState } from './utils/localStorage';
-// import mainSaga from './dataflow/saga';
 import reducer from './dataflow/reducer';
 
 function configureStore() {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  const sagaMiddleware = createSagaMiddleware();
-  const middleware = applyMiddleware(sagaMiddleware);
   const persistedState = loadState();
+  const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-  const store = createStore(reducer, persistedState, composeEnhancers(middleware));
+  const store = createStore(reducer, persistedState, reduxDevTools);
 
   // setting localStorage
   store.subscribe(throttle(() => {
     const { user } = store.getState();
     saveState({ user });
   }, 1000));
-
-  // sagaMiddleware.run(mainSaga);
 
   return store;
 }
